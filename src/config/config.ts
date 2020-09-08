53bb8eda84b62a3587722c3d1fc3d169417a125d
GerBoyle <ger_boyle@yahoo.ie>
2020-09-08 03:33:14 +0100

    1st

---
+++ src/config/config.ts
@@ -0,0 +1,19 @@
+export const config = {
+  "dev": {
+    "username": "udagramboyledev",
+    "password": "udagramboyledev",
+    "database": "udagramboyledev",
+    "host": "udagramboyledev.c2vgbioeyplt.eu-west-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "eu-west-1a",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagram-boyle-dev"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
